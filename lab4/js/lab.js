c6a71f5602449a9536c41486157e20eaafca0c78
NirvanaSkull <jwang419@ucsc.edu>
2022-04-15 11:11:18 -0700

    Update lab.js

--- lab4/js/lab.js
+++ lab4/js/lab.js
@@ -1,6 +1,18 @@
-Summary. (use period)
-Description. (use period)
-@link   URL
-@file   This files defines the MyClass class.
-@author AuthorName.
-@since  x.x.x
+  // Summary. (use period)
+  // Description. (use period)
+  // @link   URL
+  // @file   This files defines the MyClass class.
+  // @author AuthorName.
+  // @since  x.x.x
+
+// Print the board before each turn
+    // Loop through each row
+        // Loop through each space
+            // Print X, O, or Space
+            // Print a | between
+        // Print a newline at the end
+        // Print a horizontal line between
+
+        <pre><code>
+        //your code here
+        </pre></code>
