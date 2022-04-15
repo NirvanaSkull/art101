155e3a78bed1b49247d1e8aa3fbc19301a63179f
NirvanaSkull <jwang419@ucsc.edu>
2022-04-15 10:59:05 -0700

    Update lab.js

--- lab4/js/lab.js
+++ lab4/js/lab.js
@@ -1,27 +1,6 @@
-/**
- * Summary. (use period)
- *
- * Description. (use period)
- *
- * @link   URL
- * @file   This files defines the MyClass class.
- * @author AuthorName.
- * @since  x.x.x
- */
-
-/** jshint {inline configuration here} */
-
-function printBoard(board) {
-  var output = ""
-  console.log(board);
-  for (row = 0; row < 3; row++) {
-    for (space = 0; spaace < 3; space++) {
-      console.log (row * 3 + space);
-      output += board[row * 3 + spaace];
-      if (space < 2) output += '|';
-    }
-    output += '\n'
-    if (row < 2) output += '-----\n';
-  }
-  return output;
-}
+Summary. (use period)
+Description. (use period)
+@link   URL
+@file   This files defines the MyClass class.
+@author AuthorName.
+@since  x.x.x
