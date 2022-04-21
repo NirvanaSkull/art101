7042bceccb082bc5ab722ef47dea710e0b6682e8
NirvanaSkull <jwang419@ucsc.edu>
2022-04-21 10:04:28 -0700

    Update lab.js

---
+++ lab6/js/lab.js
@@ -0,0 +1,22 @@
+// Summary. (use period)
+// Description. (use period)
+// @link   URL
+// @file   This files defines the MyClass class.
+// @author Junbo Wang.
+// @since  2022.4.8
+
+// Define Variables
+var make = "Ford";
+var model = "Taurus";
+var color = "Rusty";
+var year = 1995;
+
+// Calculate
+age = 2022 - year;
+
+// Output
+document.writeln("Make:" + make + "<br>");
+document.writeln("Model:" + model + "<br>");
+document.writeln("Color:" + color + "<br>");
+document.writeln("Year:" + year + "<br>");
+document.writeln("Age:" + age + "<br>");
