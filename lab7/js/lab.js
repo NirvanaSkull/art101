009dc042ca077a8b8e57de42c2ba5e9fcf8f6b0d
NirvanaSkull <jwang419@ucsc.edu>
2022-04-26 10:07:53 -0700

    Update lab.js

--- lab7/js/lab.js
+++ lab7/js/lab.js
@@ -6,3 +6,16 @@
 // @since  2022.4.8
 
 // Define Variables
+function sortUserName() {
+  var userName = window.prompt("Hi. Please tell me you name so I can fix it.");
+  console.log("userName =", userName);
+  var nameArray = userName.split('');
+  console.log("nameArray =", nameArray);
+  var nameArraySort = nameArray.sort();
+  console.log("nameArraySort =", nameArraySort);
+  var nameSorted = nameArraySort.join('')
+  console.log("nameSorted =", nameSorted);
+  return nameSorted
+}
+
+document.writeln("Oh hey, I've fixed you name: ", sortUserName(), "</br>");
