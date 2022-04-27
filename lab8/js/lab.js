883bdb4ff9547ec7f04d19bc96331715853849db
NirvanaSkull <jwang419@ucsc.edu>
2022-04-27 09:57:04 -0700

    Update lab.js

--- lab8/js/lab.js
+++ lab8/js/lab.js
@@ -6,7 +6,23 @@
 // @since  2022.4.8
 
 // Define Variables
-function yourFunctionName (x) {
-  var results = // your operation on x here
-  return results;
+function isEven(x){
+  return (x % 2 == 0);
 }
+
+//test function
+console.log("Is 1 even? ", isEven(1))
+console.log("Is 1 even? ", isEven(2))
+
+array = [100, 81, 4,16, 42, 144, 10000]
+console.log("My array", array);
+
+var result = array.map(isEven);
+//Should return
+console.log("Test of evenness of array:", result);
+
+var result = array.map(function(x){
+  return x ** 0.5;
+})
+
+console.log("Squareroot of array:", result);
