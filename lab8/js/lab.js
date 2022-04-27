9743cdbd75457abb718c35cf3b052b5ac626b128
NirvanaSkull <jwang419@ucsc.edu>
2022-04-27 09:38:28 -0700

    Create lab.js

---
+++ lab8/js/lab.js
@@ -0,0 +1,12 @@
+// Summary. (use period)
+// Description. (use period)
+// @link   URL
+// @file   This files defines the MyClass class.
+// @author Junbo Wang.
+// @since  2022.4.8
+
+// Define Variables
+function yourFunctionName (x) {
+  var results = // your operation on x here
+  return results;
+}
