e0a9d82e683dc6c350a6ae37aaf9594dc6c0673c
NirvanaSkull <jwang419@ucsc.edu>
2022-05-03 10:22:28 -0700

    Update lab.js

--- lab9/js/lab.js
+++ lab9/js/lab.js
@@ -6,12 +6,14 @@
 // @since  2022.4.8
 
 
-var targetEl = document.getElementById("content");
-var new1El = document.createElement("button");
-targetEl.appendChild(new1El);
-targetEl.innerHTML = ("press me");
+
 
 var targetEl = document.getElementById("content");
 var new1El = document.createElement("button");
 targetEl.appendChild(new1El);
-targetEl.innerHTML = ("press Him");
+targetEl.innerHTML = ("Originally, There is a picture of a cow.");
+
+var targetEl = document.getElementById("script");
+var new1El = document.createElement("button");
+targetEl.appendChild(new1El);
+targetEl.innerHTML = ("This spot should be the output, but I replace to this sentence.");
