46a5d662c84c122440e35b5cdc9775bf135670da
NirvanaSkull <jwang419@ucsc.edu>
2022-05-10 08:54:25 -0700

    Create lab.js

---
+++ lab11/js/lab.js
@@ -0,0 +1,26 @@
+// Summary. (use period)
+// Description. (use period)
+// @link   URL
+// @file   This files defines the MyClass class.
+// @author Junbo Wang.
+// @since  2022.4.8
+
+// Define Variables
+
+
+$("button1").click(function(){
+  		// action goes here!!
+			$("#Challenges")
+			$("Challenges").html("No Events");
+
+	});
+
+$("button2").click(function(){
+	  		// action goes here!!
+					$("#Problem")
+		});
+
+$("button3").click(function(){
+		  		// action goes here!!
+						$("#Results")
+			});
