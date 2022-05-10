6c6108ae0fcdf4682b478c10cece3db9f5f90f7a
NirvanaSkull <jwang419@ucsc.edu>
2022-05-10 09:32:31 -0700

    Update lab.js

--- lab11/js/lab.js
+++ lab11/js/lab.js
@@ -10,17 +10,20 @@
 
 $("button1").click(function(){
   		// action goes here!!
-			$("#Challenges")
-			$("Challenges").html("No Events");
-
+			$('#Challenges').text('Working with my partner, experiment with basic Arrays and Objects.');
 	});
 
 $("button2").click(function(){
 	  		// action goes here!!
-					$("#Problem")
+				$("#Problem").text('Everything is ok because everything we talk about the problem in class. and all the answer has been write in the slide.');
 		});
 
 $("button3").click(function(){
 		  		// action goes here!!
-						$("#Results")
+					$("#Results").text('Everything goes well, while i play around with the element.just a syntax, after i fixed, everything is fine.');
 			});
+
+$("button4").click(function(){
+					  		// action goes here!!
+								$("#special").text('');
+						});
