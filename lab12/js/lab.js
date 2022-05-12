bf9318db71227c8bf0e35fb44bdd0448cc554efb
NirvanaSkull <jwang419@ucsc.edu>
2022-05-12 09:26:10 -0700

    Create lab.js

---
+++ lab12/js/lab.js
@@ -0,0 +1,34 @@
+// Summary. (use period)
+// Description. (use period)
+// @link   URL
+// @file   This files defines the MyClass class.
+// @author Junbo Wang.
+// @since  2022.4.8
+
+// Define Variables
+function sortingHat(str){
+  len = str.length;
+  mod = len % 4;
+  if (mod == 0) {
+    return "Gryffindor"
+  }
+  else if (mod == 1) {
+    return "Ravenclaw"
+  }
+  else if (mod == 2) {
+    return "Slytherin"
+  }
+  else if (mod == 3) {
+    return "Hufflepuff"
+  }
+}
+
+
+
+var buttonEl = document.getElementById('button');
+buttonEl.addEventListener('click', function(){
+  var name = document.getElementById('input').value;
+  var house = sortingHat(name);
+  newText= "<p> The Sorting Hat has sorted you into " + house + "</p>";
+  document.getElementById("output").innerHTML = newText;
+});
