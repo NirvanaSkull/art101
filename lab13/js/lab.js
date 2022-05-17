60fa448830e05cca5340ffa55d21279ca65f2bdf
NirvanaSkull <jwang419@ucsc.edu>
2022-05-17 10:39:11 -0700

    Update lab.js

--- lab13/js/lab.js
+++ lab13/js/lab.js
@@ -6,55 +6,81 @@
 // @since  2022.4.8
 
 // Define Variables
-function fizzBussBoom(maxNums,factorObj){
-  for (var num0; num<maxNums; num++){
-    var outputStr = "";
-    for (var factor in factorObj){
-      if (num % factor == 0){
-        outputStr += factorObj [factor];
-      }
-    }
-    if (outputStr){
-      outputStr = " - " + outputStr + "!"
-    }
-    outputToPage(num.toString() + outputStr)
-  }
-}
-function reportError(str){
-  output.innerHTML = "<div class= 'error'>" + str + "</div>"
+maxFactors = 4;
 
+outputEl = document.getElementById("output");
+
+// get the values from the webpage and write them in an object
+// this expects to have input fields with ids num0, text0, num1, text1, etc
+// returns an object that looks like this:
+//      {3: "Fizz", 5: "Buzz", 7: "Boom"}
+function getFactorObj() {
+    var factorObj = {};
+    for (var factor=0; factor<maxFactors; factor++) {
+        numId = "num" + factor;
+        textId = "text" + factor;
+        numValue = document.getElementById(numId).value;
+        textValue = document.getElementById(textId).value;
+        console.log(factor + ") num:", numValue, "text:", textValue)
+        // if either value is blank, don't use it
+        if (numValue && textValue) {
+            factorObj[numValue] = textValue;
+        }
+    }
+    return factorObj;
 }
-document.getElementById("submit").addEventListener('click', function(){
-  var max = $("max").value;
-  if (! max) {
-    reportError("You Must Provide A Maximum");
-  }
+
+function outputToPage(str) {
+    newEl = document.createElement("p");
+    newEl.innerHTML = str;
+    outputEl.appendChild(newEl);
+}
+
+// given a number and an object that looks like this:
+//      {3: "Fizz", 5: "Buzz", 7: "Boom"}
+// loops over the numbers and outputs the number and the matching text
+// for factors
+function fizzBuzzBoom(maxNums, factorObj) {
+    // iterate over all of out numbers
+    for (var num=0; num<maxNums; num++) {
+        debugger;
+        // reset output string
+        var outputStr = "";
+        // iterate over the factors we got from the html
+        for (var factor in factorObj) {
+            // check to see if this num is a multiple of factor
+            if (num % factor == 0) {
+                // if yes, than add the text to output string
+                outputStr += factorObj[factor];
+            }
+        }
+        // now if we have words in outputStr, format it like this " - FizzBuzz!"
+        if (outputStr) {
+            outputStr = " - " + outputStr + "!";
+        }
+        outputToPage(num.toString() + outputStr)
+    }
+}
+
+function reportError(str) {
+    outputEl.innerHTML = "<div class='error'>" + str + "</div>";
+}
+
+document.getElementById("submit").addEventListener("click", function() {
+    var max = document.getElementById("max").value;
+    console.log("max:", max)
+    if (! max) {
+        reportError("You must provide a maximum");
+        return;
+    }
+    var factorObj = getFactorObj();
+    console.log("factorObj:", factorObj);
+    if (Object.keys(factorObj).length === 0) {
+        reportError("You must provide at least one factor and text");
+        return;
+    }
+    // clear error if there is one
+    outputEl.innerHTML = "";
+    fizzBuzzBoom(max, factorObj);
+    outputEl.classList.add("cols");
 })
-
-
-
-
-for (i=1; i <= 200; i++) {
-    if (i%15 == 0) {
-        console.log("FizzBuzz");
-    } else if (i%3 == 0) {
-        console.log("Fizz");
-    } else if (i%5 == 0) {
-        console.log("Buzz");
-    } else if (i%7 == 0) {
-        console.log("Buzz");
-    } else {
-        console.log(i);
-    }
-}
-
-var str = 0
-str += "Fizz";
-$("#output").append("<p>" + str + "</p>");
-
-while (str) {
-    console.log("FizzBussBoom " + str);
-	str++;
-}
-
-document.getElementById("output").innerHTML = newText;
