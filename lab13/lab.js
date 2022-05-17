97c7165e78c15c2dbb006dd1bdf2f40735f99fe4
NirvanaSkull <jwang419@ucsc.edu>
2022-05-17 10:51:20 -0700

    Update lab.js

--- lab13/lab.js
+++ lab13/lab.js
@@ -6,55 +6,56 @@
 // @since  2022.4.8
 
 // Define Variables
-function fizzBussBoom(maxNums,factorObj){
-  for (var num0; num<maxNums; num++){
-    var outputStr = "";
-    for (var factor in factorObj){
-      if (num % factor == 0){
-        outputStr += factorObj [factor];
-      }
-    }
-    if (outputStr){
-      outputStr = " - " + outputStr + "!"
-    }
-    outputToPage(num.toString() + outputStr)
-  }
-}
-function reportError(str){
-  output.innerHTML = "<div class= 'error'>" + str + "</div>"
+/*
 
-}
-document.getElementById("submit").addEventListener('click', function(){
-  var max = $("max").value;
-  if (! max) {
-    reportError("You Must Provide A Maximum");
-  }
-})
+ // Define Variables
+ function fizzBussBoom(maxNums,factorObj){
+   for (var num0; num<maxNums; num++){
+     var outputStr = "";
+     for (var factor in factorObj){
+       if (num % factor == 0){
+         outputStr += factorObj [factor];
+       }
+     }
+     if (outputStr){
+       outputStr = " - " + outputStr + "!"
+     }
+     outputToPage(num.toString() + outputStr)
+   }
+ }
+ function reportError(str){
+   output.innerHTML = "<div class= 'error'>" + str + "</div>"
+
+ }
+ document.getElementById("submit").addEventListener('click', function(){
+   var max = $("max").value;
+   if (! max) {
+     reportError("You Must Provide A Maximum");
+   }
+ })
 
 
 
 
-for (i=1; i <= 200; i++) {
-    if (i%15 == 0) {
-        console.log("FizzBuzz");
-    } else if (i%3 == 0) {
-        console.log("Fizz");
-    } else if (i%5 == 0) {
-        console.log("Buzz");
-    } else if (i%7 == 0) {
-        console.log("Buzz");
-    } else {
-        console.log(i);
-    }
-}
+ for (i=1; i <= 100; i++) {
+     if (i%15 == 0) {
+         document.writeLn("FizzBuzz");
+     } else if (i%3 == 0) {
+         document.writeLn("Fizz");
+     } else if (i%5 == 0) {
+         document.writeLn("Buzz");
+     } else if (i%7 == 0) {
+         document.writeLn("Buzz");
+     } else {
+         document.writeLn(i);
+     }
+ }
 
-var str = 0
-str += "Fizz";
-$("#output").append("<p>" + str + "</p>");
+ var str = 0
+ str += "Fizz";
+ $("#output").append("<p>" + str + "</p>");
 
-while (str) {
-    console.log("FizzBussBoom " + str);
-	str++;
-}
-
-document.getElementById("output").innerHTML = newText;
+ while (str) {
+     document.writeLn("FizzBussBoom " + str);
+ 	str++;
+ }
